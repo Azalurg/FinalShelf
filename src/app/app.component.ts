5cfff475747bcb2da5235c2a32cd4be7b40955e8
Azalurg <91213389+Azalurg@users.noreply.github.com>
2024-08-17 11:31:57 +0200

    Add global style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,13 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { invoke } from "@tauri-apps/api/tauri";
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
+  imports: [CommonModule, RouterOutlet, NavbarComponent],
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
-export class AppComponent {
-  books: { title: string, genre: string, author_id: number, year: string }[] = [];
-
-  async scanDirectory(): Promise<void> {
-    try {
-      const directory = prompt('Enter directory to scan:');
-      if (directory) {
-        await invoke('tauri_scan', { directory });
-        alert('Scan completed successfully!');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      alert('Failed to scan metadata: ' + (error as Error).message);
-    }
-  }
- 
-  async getBooks(): Promise<void> {
-    try {
-      const books = await invoke<{ title: string, genre: string, author_id: number, year: string }[]>('tauri_get_books');
-      this.books = books;
-    } catch (error) {
-      console.error('Error:', error);
-      alert('Failed to retrieve books: ' + (error as Error).message);
-    }
-  }
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from "@angular/router";
 
 export const routes: Routes = [];
+
+
+
+// import { HomeComponent } from './home/home.component';
+// import { MarketComponent } from './market/market.component';
+// import { ProfileComponent } from './profile/profile.component';
+// import { TransactionsComponent } from './transactions/transactions.component';
+
+// const routes: Routes = [
+//   {
+//     path: '',
+//     component: HomeComponent,
+//     data: {}
+//   },
+//   {
+//     path: 'market',
+//     component: MarketComponent
+//   },
+//   {
+//     path: 'profile',
+//     component: ProfileComponent
+//   },
+//   {
+//     path: 'transactions',
+//     component: TransactionsComponent
+//   }
